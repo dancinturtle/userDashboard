539b5c6d3e7c6956caa7ee72ac47aa87eb3f2353
dancinturtle <amy.giver@gmail.com>
2016-04-19 20:42:21 -0700

    I changed some stuff, not much

--- application/views/create.php
+++ application/views/create.php
@@ -6,5 +6,20 @@
 </head>
 <body>
 <?php $this->load->view('partials/navbarLogOff');?>
+	<div class="container">
+		<h2>Add a new user</h2>
+		<form role="form" action="/signins/admin" method="post">
+			<div class="form-group">
+				<label for="email">Email address:</label>
+				<input type="text" name="email" class="form-control">
+			</div>
+			<div class="form-group">
+				<label for="password">Password:</label>
+				<input type="password" name="password" class="form-control">
+			</div>
+			<button type="submit" class="btn btn-success">Sign in</button>
+		</form>
+		
+	</div>
 </body>
 </html>
